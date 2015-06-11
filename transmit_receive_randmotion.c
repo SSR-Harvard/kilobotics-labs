428fe0c2efeae1c3c75e93282135b4e6e6854c77
Self-Organizing Systems Research Group <SSR-Harvard@users.noreply.github.com>
2015-06-11 15:54:04 -0400

    Update transmit_receive_randmotion.c

--- transmit_receive_randmotion.c
+++ transmit_receive_randmotion.c
@@ -13,6 +13,7 @@ uint32_t last_motion_update = 0;
 // Function to set new motion.
 void set_motion(uint8_t new_motion)
 {
+	// We only need to take an action if the motion is being changed.
 	if (current_motion != new_motion)
 	{
 		current_motion = new_motion;
