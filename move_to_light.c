39b6b6333354de1fad5a16fbec110bcb4fcf9b88
Self-Organizing Systems Research Group <SSR-Harvard@users.noreply.github.com>
2015-06-11 17:36:07 -0400

    Update move_to_light.c

--- move_to_light.c
+++ move_to_light.c
@@ -3,64 +3,49 @@
 #define LEFT 0
 #define RIGHT 1
 
-// declare constants
-static const uint16_t THRESH_HI = 600;
-static const uint16_t THRESH_LO = 300;
+#define THRESH_LO = 300;
+#define THRESH_HI = 600;
 
-// declare variables
-uint8_t cur_direction;
-int16_t cur_light = 0;
+int current_direction;
+int current_light = 0;
 
-// To reduce noise we take the average of 300 samples, discarding
-// samples where the ADC sensor failed (returned -1).
-void sample_light() {
-    int16_t numsamples = 0;
-    long average = 0;
+// The ambient light sensor gives noisy readings. To mitigate this,
+// we take the average of 300 samples in quick succession.
+void sample_light()
+{
+    int number_of_samples = 0;
+    int sum = 0;
 
-    while (numsamples < 300) {
-        int16_t sample = get_ambientlight();
-        if (sample != -1) {
-            average += sample;
-            numsamples++;
+    while (number_of_samples < 300) {
+        int sample = get_ambientlight();
+        
+        // -1 indicate a failed sample, which should be discarded.
+        if (sample != -1)
+        {
+            average = average + sample;
+            number_of_samples = number_of_samples + 1;
         }
     }
 
-    cur_light = average / 300;
-}
-
-void turn_left() {
-    spinup_motors();
-    set_motors(kilo_turn_left, 0);
-    set_color(RGB(2,0,0));
-}
-
-void turn_right() {
-    spinup_motors();
-    set_motors(0, kilo_turn_right);
-    set_color(RGB(0,2,0));
+    current_light = sum / number_of_samples;
 }
 
 
-void setup() {
-    cur_direction = LEFT;
-    turn_left();
+
+void setup()
+{
 }
 
 void loop() {
     sample_light();
-    switch(cur_direction) {
-        case LEFT:
-            if (cur_light < THRESH_LO) {
-                cur_direction = RIGHT;
-                turn_right();
-            }
-            break;;
-        case RIGHT:
-            if (cur_light > THRESH_HI) {
-                cur_direction = LEFT;
-                turn_left();
-            }
-            break;
+    
+    if (current_light < THRESH_LO)
+    {
+        set_motion(RIGHT);
+    }
+    else if (current_light > THRESH_HI)
+    {
+        set_motion(LEFT);
     }
 }
 
