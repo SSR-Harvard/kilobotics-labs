0cf7063d3646277a8ad7fc2fc67882665e2b0e82
Self-Organizing Systems Research Group <SSR-Harvard@users.noreply.github.com>
2015-06-11 16:45:15 -0400

    Update test_listener.c

--- test_listener.c
+++ test_listener.c
@@ -8,31 +8,29 @@ void setup()
 
 void loop()
 {
-	// Blink LED yellow whenever a message is received.
-	if (new_message == 1)
-	{
-		// Reset flag so LED is only blinked once per message.
-		new_message = 0;
-		
-		set_color(RGB(1, 1, 0));
-		delay(100);
-		set_color(RGB(0, 0, 0));
-	}
+    // Blink LED yellow whenever a message is received.
+    if (new_message == 1)
+    {
+        // Reset flag so LED is only blinked once per message.
+        new_message = 0;
+        
+        set_color(RGB(1, 1, 0));
+        delay(100);
+        set_color(RGB(0, 0, 0));
+    }
 }
 
 // Set flag on message reception.
 void message_rx(message_t *message, distance_measurement_t *distance)
 {
-	new_message = 1;
+    new_message = 1;
 }
 
 int main() {
-	kilo_init();
-	
-	// Register message_rx callback function.
-	kilo_message_rx = message_rx;
-	
-	kilo_start(setup, loop);
-	
-	return 0;
+    kilo_init();
+    // Register message_rx callback function.
+    kilo_message_rx = message_rx;
+    kilo_start(setup, loop);
+    
+    return 0;
 }
