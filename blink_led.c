0a18c9a2ada58e3106f1dbcaf9070307b6217461
Self-Organizing Systems Research Group <SSR-Harvard@users.noreply.github.com>
2015-06-11 16:50:04 -0400

    Update blink_led.c

--- blink_led.c
+++ blink_led.c
@@ -2,29 +2,29 @@
 
 void setup()
 {
-	// Put your setup code here. This is run once before entering the loop.
+    // Put any setup code here. This is run once before entering the loop.
 }
 
 void loop()
 {
-	// Put your main code here. This is run repeatedly.
-
-	// Turn LED blue.
-	set_color(RGB(0, 0, 1));
-	// Wait half a second (500 ms).
-	delay(500);
-	// Turn LED red.
-	set_color(RGB(1, 0, 0));
-	// Wait half a second (500 ms).
-	delay(500);
+    // Put the main code here. This is run repeatedly.
+    
+    // Turn LED blue.
+    set_color(RGB(0, 0, 1));
+    // Wait half a second (500 ms).
+    delay(500);
+    // Turn LED red.
+    set_color(RGB(1, 0, 0));
+    // Wait half a second (500 ms).
+    delay(500);
 }
 
 int main()
 {
-	// Initialize hardware.
-	kilo_init();
-	// Register your program.
-	kilo_start(setup, loop);
-
-	return 0;
+    // Initialize hardware.
+    kilo_init();
+    // Register the program.
+    kilo_start(setup, loop);
+    
+    return 0;
 }
