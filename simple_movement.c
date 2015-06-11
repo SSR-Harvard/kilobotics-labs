e43dd64c4188834128c7819c3c121d4dcb1370ac
Self-Organizing Systems Research Group <SSR-Harvard@users.noreply.github.com>
2015-06-11 16:51:31 -0400

    Update simple_movement.c

--- simple_movement.c
+++ simple_movement.c
@@ -6,35 +6,35 @@ void setup()
 
 void loop()
 {
-	// Turn LED green.
-	set_color(RGB(0, 1, 0));
-	// Spinup motors to overcome friction.
-	spinup_motors();
-	// Move straight for 2 seconds (2000 ms).
-	set_motors(kilo_straight_left, kilo_straight_right);
-	delay(2000);
-
-	// Turn LED red.
-	set_color(RGB(1, 0, 0));
-	// Spinup motors to overcome friction.
-	spinup_motors();
-	// Turn left for 2 seconds (2000 ms).
-	set_motors(kilo_turn_left, 0);
-	delay(2000);
-
-	// Turn LED blue.
-	set_color(RGB(0, 0, 1));
-	// Spinup motors to overcome friction.
-	spinup_motors();
-	// Turn right for 2 seconds (2000 ms).
-	set_motors(0, kilo_turn_right);
-	delay(2000);
-
-	// Turn off LED.
-	set_color(RGB(0, 0, 0));
-	// Stop for half a second (500 ms).
-	set_motors(0, 0);
-	delay(500);
+    // Set LED green.
+    set_color(RGB(0, 1, 0));
+    // Spinup motors to overcome friction.
+    spinup_motors();
+    // Move straight for 2 seconds (2000 ms).
+    set_motors(kilo_straight_left, kilo_straight_right);
+    delay(2000);
+    
+    // Set LED red.
+    set_color(RGB(1, 0, 0));
+    // Spinup motors to overcome friction.
+    spinup_motors();
+    // Turn left for 2 seconds (2000 ms).
+    set_motors(kilo_turn_left, 0);
+    delay(2000);
+    
+    // Set LED blue.
+    set_color(RGB(0, 0, 1));
+    // Spinup motors to overcome friction.
+    spinup_motors();
+    // Turn right for 2 seconds (2000 ms).
+    set_motors(0, kilo_turn_right);
+    delay(2000);
+    
+    // Set LED off.
+    set_color(RGB(0, 0, 0));
+    // Stop for half a second (500 ms).
+    set_motors(0, 0);
+    delay(500);
 }
 
 int main()
