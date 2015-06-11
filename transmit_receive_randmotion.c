6477f340b7108f52b11e17de0af2a22f5c48a099
Self-Organizing Systems Research Group <SSR-Harvard@users.noreply.github.com>
2015-06-11 17:02:03 -0400

    Update transmit_receive_randmotion.c

--- transmit_receive_randmotion.c
+++ transmit_receive_randmotion.c
@@ -13,106 +13,106 @@ uint32_t last_motion_update = 0;
 // Function to handle motion.
 void set_motion(uint8_t new_motion)
 {
-	// We only need to take an action if the motion is being changed.
-	if (current_motion != new_motion)
-	{
-		current_motion = new_motion;
-
-		if (current_motion == STOP)
-		{
-			set_motors(0, 0);
-		}
-		else if (current_motion == FORWARD)
-		{
-			spinup_motors();
-			set_motors(kilo_straight_left, kilo_straight_right);
-		}
-		else if (current_motion == LEFT)
-		{
-			spinup_motors();
-			set_motors(kilo_turn_left, 0);
-		}
-		else if (current_motion == RIGHT)
-		{
-			spinup_motors();
-			set_motors(0, kilo_turn_right);
-		}
-	}
+    // We only need to take an action if the motion is being changed.
+    if (current_motion != new_motion)
+    {
+        current_motion = new_motion;
+        
+        if (current_motion == STOP)
+        {
+            set_motors(0, 0);
+        }
+        else if (current_motion == FORWARD)
+        {
+            spinup_motors();
+            set_motors(kilo_straight_left, kilo_straight_right);
+        }
+        else if (current_motion == LEFT)
+        {
+            spinup_motors();
+            set_motors(kilo_turn_left, 0);
+        }
+        else if (current_motion == RIGHT)
+        {
+            spinup_motors();
+            set_motors(0, kilo_turn_right);
+        }
+    }
 }
 
 void setup()
 {
-	// Initialize empty message.
-	message.type = NORMAL;
-	message.crc = message_crc(&message);
+    // Initialize empty message.
+    message.type = NORMAL;
+    message.crc = message_crc(&message);
 }
 
 void loop()
 {
-	if (kilo_ticks > last_motion_update + 32)
-	{
-		last_motion_update = kilo_ticks;
-		
-		// If a message was received in the last second, set a random motion.
-		if (new_message == 1)
-		{
-			new_message = 0;
-
-			// Generate an 8-bit random number (between 0 and 255).
-			int random_number = rand_hard();
-
-			// Compute the remainder of the random number when divided by 4.
-			// This gives a new random number in the set {0, 1, 2, 3}.
-			int random_direction = (random_number % 4);
-
-			// There is a 50% chance of random_direction being 0 OR 1, in which
-			// case set LED green and move forward.
-			if ((random_direction == 0) || (random_direction == 1))
-			{
-				set_color(RGB(0, 1, 0));
-				set_motion(FORWARD);
-			}
-			// There is a 25% chance of random_direction being 2, in which case
-			// set LED red and move left.
-			else if (random_direction == 2)
-			{
-				set_color(RGB(1, 0, 0));
-				set_motion(LEFT);
-			}
-			// There is a 25% chance of random_direction being 3, in which case
-			// set LED blue and move right.
-			else if (random_direction == 3)
-			{
-				set_color(RGB(0, 0, 1));
-				set_motion(RIGHT);
-			}
-		}
-		// If no message was received in the last second, set LED white and
-		// stop moving.
-		else
-		{
-			set_color(RGB(1, 1, 1));
-			set_motion(STOP);
-		}
-	}
+    if (kilo_ticks > last_motion_update + 32)
+    {
+        last_motion_update = kilo_ticks;
+        
+        // If a message was received in the last second, set a random motion.
+        if (new_message == 1)
+        {
+            new_message = 0;
+            
+            // Generate an 8-bit random number (between 0 and 255).
+            int random_number = rand_hard();
+            
+            // Compute the remainder of the random number when divided by 4.
+            // This gives a new random number in the set {0, 1, 2, 3}.
+            int random_direction = (random_number % 4);
+            
+            // There is a 50% chance of random_direction being 0 OR 1, in which
+            // case set LED green and move forward.
+            if ((random_direction == 0) || (random_direction == 1))
+            {
+                set_color(RGB(0, 1, 0));
+                set_motion(FORWARD);
+            }
+            // There is a 25% chance of random_direction being 2, in which case
+            // set LED red and move left.
+            else if (random_direction == 2)
+            {
+                set_color(RGB(1, 0, 0));
+                set_motion(LEFT);
+            }
+            // There is a 25% chance of random_direction being 3, in which case
+            // set LED blue and move right.
+            else if (random_direction == 3)
+            {
+                set_color(RGB(0, 0, 1));
+                set_motion(RIGHT);
+            }
+        }
+        // If no message was received in the last second, set LED white and
+        // stop moving.
+        else
+        {
+            set_color(RGB(1, 1, 1));
+            set_motion(STOP);
+        }
+    }
 }
 
 void message_rx(message_t *m, distance_measurement_t *d)
 {
-	new_message = 1;
+new_me  ssage = 1;
 }
 
 message_t *message_tx()
 {
-	return &message;
+return &    message;
 }
 
 int main()
 {
-	kilo_init();
-	kilo_message_rx = message_rx;
-	kilo_message_tx = message_tx;
-	kilo_start(setup, loop);
-
-	return 0;
+    kilo_init();
+    kilo_message_rx = message_rx;
+    kilo_message_tx = message_tx;
+    kilo_start(setup, loop);
+    
+    return 0;
 }
