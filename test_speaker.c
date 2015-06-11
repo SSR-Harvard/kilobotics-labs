eb982e7089f9b097b1470986c102960c0f329110
Self-Organizing Systems Research Group <SSR-Harvard@users.noreply.github.com>
2015-06-11 14:25:29 -0400

    Update test_speaker.c

--- test_speaker.c
+++ test_speaker.c
@@ -2,26 +2,30 @@
 
 message_t message;
 
-void setup() {
-  // Initialize message:
-  
-  // The type is almost always NORMAL.
-  message.type = NORMAL;
-
-  // Some dummy data as an example.
-  message.data[0] = 0;
-
-  // It's important that the CRC is computed
-  // *after* all the data has been set (otherwise it will be wrong).
-  message.crc = message_crc(&message);
+void setup()
+{
+	// Initialize message:
+	
+	// The type is always NORMAL.
+	message.type = NORMAL;
+	
+	// Some dummy data as an example.
+	message.data[0] = 0;
+	
+	// It's important that the CRC is computed *after* the data has been set;
+	// otherwise it will be wrong.
+	message.crc = message_crc(&message);
 }
 
 void loop()
 {
+	// No need for any code here in this case. Everything is being taken care
+	// of by the message_tx and message_tx_success callback functions.
 }
 
-message_t *message_tx() {
-  return &message;
+message_t *message_tx()
+{
+	return &message;
 }
 
 void message_tx_success()
@@ -32,15 +36,16 @@ void message_tx_success()
 	set_color(RGB(0,0,0));
 }
 
-int main() {
-  kilo_init();
-
-  // Register message_tx function.
-  kilo_message_tx = message_tx;
-  // Register message_tx_success function.
-  kilo_message_tx_success = message_tx_success;
-
-  kilo_start(setup, loop);
-
-  return 0;
+int main()
+{
+	kilo_init();
+	
+	// Register message_tx function.
+	kilo_message_tx = message_tx;
+	// Register message_tx_success function.
+	kilo_message_tx_success = message_tx_success;
+	
+	kilo_start(setup, loop);
+	
+	return 0;
 }
