2034bfdd822407c341c119d1a21f28b937ed0183
Self-Organizing Systems Research Group <SSR-Harvard@users.noreply.github.com>
2015-06-11 14:53:31 -0400

    Update test_listener.c

--- test_listener.c
+++ test_listener.c
@@ -1,32 +1,37 @@
 #include <kilolib.h>
 
-// declare variables
 uint8_t new_message = 0;
 
-// no setup code required
-void setup() { }
+void setup()
+{
+}
 
-void loop() {
-    // Blink yellow when on message received
-    if (new_message) {
+void loop()
+{
+    // Blink LED yellow whenever a message is received.
+    if (new_message == 1)
+    {
+        // Reset flag so LED is only blinked once per message.
         new_message = 0;
-        set_color(RGB(1,1,0));
+        
+        set_color(RGB(1, 1, 0));
         delay(100);
-        set_color(RGB(0,0,0));
+        set_color(RGB(0, 0, 0));
     }
 }
 
-// turn flag on message reception
-void message_rx(message_t *m, distance_measurement_t *d) {
+// Set flag on message reception.
+void message_rx(message_t *message, distance_measurement_t *distance)
+{
     new_message = 1;
 }
 
 int main() {
-    // initialize hardware
     kilo_init();
-    // register message reception callback
+
+    // Register message_rx callback function.
     kilo_message_rx = message_rx;
-    // register your program
+
     kilo_start(setup, loop);
 
     return 0;
