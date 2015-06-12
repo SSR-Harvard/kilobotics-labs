ad5a08c431c408ee54a323dd079ff04849cb5b1b
Self-Organizing Systems Research Group <SSR-Harvard@users.noreply.github.com>
2015-06-12 14:51:19 -0400

    Update gradient_simple.c

--- gradient_simple.c
+++ gradient_simple.c
@@ -1,8 +1,9 @@
 #include <kilolib.h>
 
-// Maximum of an 8-bit number (since we're using 1 byte of the message to send
-// the gradient value).
+// Since we're using one byte (8 bits) of the message to communicate gradients,
+// the maximum possible gradient is 2^8 - 1 = 255.
 #define GRADIENT_MAX 255
+#define SEED_ID 42
 
 int own_gradient = GRADIENT_MAX;
 int received_gradient = 0;
@@ -11,36 +12,41 @@ message_t message;
 
 void setup()
 {
-    // If the ID is 42, the robot is the seed its gradient should be 0 rather
-    // than GRADIENT_MAX.
-    if (kilo_uid == 42)
+    // If the ID is SEED_ID, the robot is the seed and its gradient should be 0:
+    // overwrite the previously set value of GRADIENT_MAX.
+    if (kilo_uid == SEED_ID)
     {
         own_gradient = 0;
     }
     
-    // Compute the message.
+    // Set the transmission message.
     message.type = NORMAL;
     message.data[0] = own_gradient;
     message.crc = message_crc(&message);
 }
 
 void loop() {
-    if (new_message == 1)
+    if (kilo_uid != SEED_ID)
     {
-        new_message = 0;
-        
-        if (own_gradient > received_gradient + 1)
+        if (new_message == 1)
         {
-            own_gradient = received_gradient + 1;
+            new_message = 0;
             
-            // Update the message whenever the gradient changes.
-            message.type = NORMAL;
-            message.data[0] = own_gradient;
-            message.crc = message_crc(&message);
+            // If a neighbor has a gradient that is 2 or more less than my own,
+            // the gradient needs to be reduced to its gradient + 1.
+            if (own_gradient > received_gradient + 1)
+            {
+                own_gradient = received_gradient + 1;
+                
+                // Update the trasmission message whenever the gradient changes.
+                message.type = NORMAL;
+                message.data[0] = own_gradient;
+                message.crc = message_crc(&message);
+            }
         }
     }
     
-    // Set LED color based on gradient.
+    // Set the LED color based on the gradient.
     if (own_gradient == 0)
     {
         set_color(RGB(1, 1, 1)); // White
