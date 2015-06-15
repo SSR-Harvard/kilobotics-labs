c6137854aa41f82072f2977f5a5af371e246735a
Self-Organizing Systems Research Group <SSR-Harvard@users.noreply.github.com>
2015-06-15 13:10:50 -0400

    Update simple_movement.c

--- simple_movement.c
+++ simple_movement.c
@@ -6,31 +6,31 @@ void setup()
 
 void loop()
 {
-    // Set LED green.
+    // Set the LED green.
     set_color(RGB(0, 1, 0));
-    // Spinup motors to overcome friction.
+    // Spinup the motors to overcome friction.
     spinup_motors();
     // Move straight for 2 seconds (2000 ms).
     set_motors(kilo_straight_left, kilo_straight_right);
     delay(2000);
     
-    // Set LED red.
+    // Set the LED red.
     set_color(RGB(1, 0, 0));
-    // Spinup motors to overcome friction.
+    // Spinup the motors to overcome friction.
     spinup_motors();
     // Turn left for 2 seconds (2000 ms).
     set_motors(kilo_turn_left, 0);
     delay(2000);
     
-    // Set LED blue.
+    // Set the LED blue.
     set_color(RGB(0, 0, 1));
-    // Spinup motors to overcome friction.
+    // Spinup the motors to overcome friction.
     spinup_motors();
     // Turn right for 2 seconds (2000 ms).
     set_motors(0, kilo_turn_right);
     delay(2000);
     
-    // Set LED off.
+    // Set the LED off.
     set_color(RGB(0, 0, 0));
     // Stop for half a second (500 ms).
     set_motors(0, 0);
