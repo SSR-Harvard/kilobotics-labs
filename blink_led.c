545d83890e88664b3b95808ec977868d971ab984
Self-Organizing Systems Research Group <SSR-Harvard@users.noreply.github.com>
2015-06-15 12:09:02 -0400

    Update blink_led.c

--- blink_led.c
+++ blink_led.c
@@ -9,12 +9,12 @@ void loop()
 {
     // Put the main code here. This is run repeatedly.
     
-    // Set LED blue.
-    set_color(RGB(0, 0, 1));
+    // Set the LED red.
+    set_color(RGB(1, 0, 0));
     // Wait half a second (500 ms).
     delay(500);
-    // Set LED red.
-    set_color(RGB(1, 0, 0));
+    // Set the LED blue.
+    set_color(RGB(0, 0, 1));
     // Wait half a second (500 ms).
     delay(500);
 }
